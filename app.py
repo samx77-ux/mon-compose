810e8f21efee1f3e4f136b56dd67ce31f6793838
sams <samirechikr77@gmail.com>
2026-03-27 21:31:44 +0100

    premier commit

---
+++ app.py
@@ -0,0 +1,22 @@
+import os
+import psycopg2
+from http.server import HTTPServer, BaseHTTPRequestHandler
+
+class Handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        conn = psycopg2.connect(
+            host=os.environ["DB_HOST"],
+            dbname=os.environ["DB_NAME"],
+            user=os.environ["DB_USER"],
+            password=os.environ["DB_PASSWORD"]
+        )
+        cur = conn.cursor()
+        cur.execute("SELECT version();")
+        version = cur.fetchone()[0]
+        conn.close()
+
+        self.send_response(200)
+        self.end_headers()
+        self.wfile.write(f"DB connectee : {version}".encode())
+
+HTTPServer(("0.0.0.0", 8000), Handler).serve_forever()
